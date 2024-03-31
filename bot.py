231c464332569f4e8405d83743addb109b1289a0
bbotexx <158470769+bbotexx@users.noreply.github.com>
2024-04-01 03:58:02 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import config
+import telebot
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+bot = telebot.TeleBot(config.token)
+rpc_connection = AuthServiceProxy("http://user:password@localhost:8332/")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот, который повторяет написанное сообщение. "
+                          "Для получения нового адреса введи /getnewaddress, для получения баланса - /getbalance.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['getnewaddress'])
+def get_new_address(message):
+    new_address = rpc_connection.getnewaddress()
+    bot.reply_to(message, f"Новый адрес: {new_address}")
+
+@bot.message_handler(commands=['getbalance'])
+def get_balance(message):
+    balance = rpc_connection.getbalance()
+    bot.reply_to(message, f"Баланс кошелька: {balance}")
+
+if _name_ == "_main_":
+    bot.infinity_polling()
